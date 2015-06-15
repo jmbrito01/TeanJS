c9a3ad71edb7829ec225a7f73fe1b7cfa4390634
João Marcelo Brito <jmbrito1996@hotmail.com>
2015-06-14 21:21:24 -0300

    Version 1.0.1
    Check changelog for more information.

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 ﻿# TeanJS
 
-This is a simple NodeJS module for getting the stock and forex quotes in a 15 minute max delay.
-This library only encapsulates Yahoo-Finance and Yahoo-Currency.
-
+This is a simple NodeJS module that encapsulated yahoo-finance, yahoo-currency and some 
+math functions in a powerfull and fast technical analysis package.
 We plan on later versions add technical analysis and chart functions.
 
-Changelog:
+#Changelog:
 V1.0.0
-	-> Forex Snapshots
-	-> Quote Snapshots
-	-> Quote Historical Values
+	-> Forex Snapshots.
+	-> Quote Snapshots.
+	-> Quote Historical Values.
+V1.0.1
+	-> Added StockWatcher.getMax.
+	-> Added StockWatcher.getMin.
+	-> Added StockWatcher.getSMA.
+	-> Added JSDoc to the project.
+
+#Dependencies
+	JSDoc, yahoo-currency, yahoo-finance, promise.
 
 License: MIT
--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-﻿/* createForexWatch function 
- * Description: Creates a forex watch
- * Params: 
- *      quote: The symbol of the forex currency (Ex: 'EURUSD')
- * Return: 
- *      An object to handle this forex currency
+﻿/* @module tean-js
+ * @author João Marcelo Brito
+ * 
+ * Creates a forex watch
+ * @param {string} quote - The symbol of the forex currency (Ex: 'EURUSD')
+ * @returns {ForexWatcher} the ForexWatcher object
  */      
-exports.createForexWatch = function (quote) {
+createForexWatch = function (quote) {
     var fx = require('yahoo-currency');
+    /*@class watcher*/
     var watcher = {
         symbol : quote
     };
@@ -19,28 +20,21 @@ exports.createForexWatch = function (quote) {
     return watcher;
 }
 
-/* createStockWatch function
- * Description: Creates a stock watcher
- * Params:
- *      quote: The symbol of the stock (Ex AAPL)
- *      interval: 'd' = Day, 'w' = Week, 'm' = Month
- * Return:
- *      An object to handle this stock or null if error */
-exports.createStockWatch = function (quote, interval) {
+/* Creates a stock watcher
+ * @param {string} quote - The symbol of the stock (Ex AAPL)
+ * @param {integer} interval -'d' = Day, 'w' = Week, 'm' = Month
+ * @returns {StockWatcher} the StockWatcher object
+ */
+createStockWatch = function (quote, interval) {
     var watcher = {
         symbol: quote, 
         interval: interval
     };
-    var finance = require('yahoo-finance');
-    var timeseries = require('timeseries-analysis');
-    
-    /* getSnapshot function
-     * Description: Retrieves a snapshot object of the stock
-     * Params:
-     *      cb: a callback with 2 parameters
-     *          err: null is no error, or the error itself.
-     *          snapshot: An object containing the stock informations.
-     *        
+    var finance = require('yahoo-finance');   
+    var Promise = require('promise');
+    /* Retrieves a snapshot object of the stock
+     * @function getSnapshot
+     * @param {callback} cb: a callback with 2 parameters     
      * */
     watcher.getSnapshot = function (cb){
         finance.snapshot({
@@ -50,14 +44,11 @@ exports.createStockWatch = function (quote, interval) {
             cb(err, snapshot);
         });
     }
-    /* getHistorical function
-     * Description: Retrieves an array of objects containing the historical values of the stock
-     * Params:
-     *      from: Start period Ex: '2015-06-01'
-     *      to: End period Ex: '2015-06-14'
-     *      cb: callback function with 2 params
-     *          err: null if no error, the error itself otherwise.
-     *          quotes: An array of objects containing the historical values.
+    /* Retrieves an array of objects containing the historical values of the stock
+     * 
+     * @param {Date} from - Start period Ex: '2015-06-01'
+     * @param {Date} end = period Ex: '2015-06-14'
+     * @param {callback} cb - callback function with 2 params
      */
     watcher.getHistorical = function (from, to, cb) {
         finance.historical({
@@ -69,5 +60,77 @@ exports.createStockWatch = function (quote, interval) {
             cb(err, quotes);
         });
     }
+    /* Gets the max close price from the period 
+     * @param {integer} period - period of the max price search
+     * @param {callback} cb - callback with the result
+     */
+    watcher.getMax = function (period, cb) {
+        if (watcher.interval == 'd')
+            var from = new Date((new Date()).getTime() - (new Date(period * 24 * 60 * 60 * 1000)));
+        else if (watcher.interval == 'w')
+            var from = new Date((new Date()).getTime() - (new Date(period * 7 * 24 * 60 * 60 * 1000)));
+        else if (watcher.interval == 'm')
+            var from = new Date((new Date()).getTime() - (new Date(period * 30 * 24 * 60 * 60 * 1000)));
+        watcher.getHistorical(from, new Date(), function (err, quotes) {
+            if (err == null)
+                cb(null);
+            var max = 0;
+            for (var i = 0; i < quotes.length; i++) 
+                if (quotes[i].close > max)
+                    max = quotes[i].close;
+            cb(max);
+        });
+    }
+    /* Gets the min close price from the period 
+     * @param {integer} period - period of the min price search
+     * @param {callback} cb - callback with the result
+     */
+    watcher.getMin = function (period, cb) {
+        if (watcher.interval == 'd')
+            var from = new Date((new Date()).getTime() - (new Date(period * 24 * 60 * 60 * 1000)));
+        else if (watcher.interval == 'w')
+            var from = new Date((new Date()).getTime() - (new Date(period * 7 * 24 * 60 * 60 * 1000)));
+        else if (watcher.interval == 'm')
+            var from = new Date((new Date()).getTime() - (new Date(period * 30 * 24 * 60 * 60 * 1000)));
+        watcher.getHistorical(from, new Date(), function (err, quotes) {
+            if (err == null)
+                cb(null);
+            var min = watcher.getMax();
+            for (var i = 0; i < quotes.length; i++)
+                if (quotes[i].close < min)
+                    max = quotes[i].close;
+            cb(max);
+        });
+    }
+    /* Gets the SMA close price from the period 
+     * @param {integer} period - period of the SMA price search
+     * @returns {integer} the SMA of the period.
+     */
+    watcher.getSMA = function (period) {
+        if (watcher.interval == 'd')
+            var from = new Date((new Date()).getTime() - (new Date(period * 24 * 60 * 60 * 1000)));
+        else if (watcher.interval == 'w')
+            var from = new Date((new Date()).getTime() - (new Date(period * 7 * 24 * 60 * 60 * 1000)));
+        else if (watcher.interval == 'm')
+            var from = new Date((new Date()).getTime() - (new Date(period * 30 * 24 * 60 * 60 * 1000)));
+        return new Promise(function (fulfill, reject) {
+            watcher.getHistorical(from, new Date(), function (err, quotes) {
+                if (err != null)
+                    reject(err);
+                var sum = 0;
+                for (var i = 0; i < quotes.length; i++) {
+                    sum = sum + quotes[i].close;
+                    if (i == (quotes.length - 1))
+                        fulfill(sum/quotes.length)
+                };
+
+            });
+        });
+    }
     return watcher;
 }
+
+var watcher = createStockWatch('WMT', 'd');
+watcher.getSMA(10).then(function (sma) {
+    console.log(sma);
+});
